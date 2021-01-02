3eb476b33fdfdd3ecd02d6e638994e1c87bd7bdd
Ilya Yarusov <originalyia@gmail.com>
2021-01-03 01:10:20 +0800

    create new package: "calculations"

---
+++ calculations/mathem.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from typing import Union
+
+
+def custom_sum(a: Union[int, str], b: int = 10) -> int:
+    return a + b
+import os
+
+
---
+++ calculations/mathem.pyi
@@ -0,0 +1,4 @@
+from typing import Union
+def custom_sum(a: Union[int, str], b: int) -> int: ...
+
+
