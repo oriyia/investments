530394f36236cc12e158dc5b1e64eea75cb23364
Ilya Yarusov <originalyia@gmail.com>
2021-01-04 16:27:49 +0800

    all add from portfolio.py to main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 """
 Описание всего модуля. Автоматически будет записано в атрибут doc
 """
-from loader.portfolio import get_api_token
-import os
+from loader.portfolio import *
 
-tk_uuc = get_api_token('ps_uuc')
-print(tk_uuc)
+settings_file = 'settings.yml'
+client = connection(settings_file)
+accounts_id = client.user.user_accounts_get().payload.accounts
+bk_account_id = accounts_id[0].broker_account_id
+uuc_account_id = accounts_id[1].broker_account_id
+
+
+def get_all(clients, broker_account_id):
+    return free_money(clients, broker_account_id), load_portfolio(clients, broker_account_id)
+
+
+usd, eur = exchange_rates(client)
+cash, portfolio = get_all(client, uuc_account_id)
+money = create_table_money(usd, eur, cash)
+table_portfolio = create_table_portfolio(client, portfolio, usd, eur)
+print(table_portfolio)
