12868879fae7843174aad8641064122b19001e18
Ilya Yarusov <originalyia@gmail.com>
2021-01-04 09:32:57 +0800

    apply modyle in main

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 """
 Описание всего модуля. Автоматически будет записано в атрибут doc
 """
-from calculations.mathem import custom_sum
-import yaml
-
+from loader.portfolio import get_api_token
 import os
 
-print(os.getenv('pswrd'))
-
-
-def yaml_loader(file):
-    with open(file, 'r') as f:
-        data = yaml.load(f, Loader=yaml.BaseLoader)
-    return data
-
-
-if __name__ == '__main__':
-    result = custom_sum(3, 1)
-    print(result)
-    a = yaml_loader('C:/Users/Ilya/settings/config')
-    print(a['api_tkn']['tkn_ucc'])
+tk_uuc = get_api_token('ps_uuc')
+print(tk_uuc)
